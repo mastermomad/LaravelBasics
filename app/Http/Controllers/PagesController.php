f4720affb846d3adba2d1653b3d3a6ac9ff66936
mastermomad <mf@integramme.com>
2019-06-03 17:13:06 +0800

    add return view to function home which display home page from PagesController

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -5,6 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
-{
-    //
+{    
+    //create a function  to return view
+    public function home(){
+    	$tasks=[
+     'Go To Market',
+     'Go To Store',
+     'Go To Work'    
+    ];
+    	return view('welcome',[
+            'foo'=>'bar',
+            'tasks'=>$tasks
+    	]);
+    }
 }
